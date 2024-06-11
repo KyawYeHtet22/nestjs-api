168b2bfa6af5cca889996e8340c0aa6b1053a8be
Kyaw Ye Htet <kyawyehtet@amdon.com>
2024-06-12 00:32:33 +0630

    book controller testcase error fix

--- src/book/book.controller.spec.ts
+++ src/book/book.controller.spec.ts
@@ -3,6 +3,8 @@ import { BookController } from './book.controller';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { Category } from './schemas/book.schema';
+import { AuthGuard } from '../auth/auth.guard';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 describe('BookController', () => {
   let controller: BookController;
@@ -23,7 +25,12 @@ describe('BookController', () => {
           },
         },
       ],
-    }).compile();
+    })
+      .overrideGuard(AuthGuard)
+      .useValue({
+        canActivate: jest.fn(),
+      })
+      .compile();
 
     controller = module.get<BookController>(BookController);
     service = module.get<BookService>(BookService);
@@ -41,31 +48,43 @@ describe('BookController', () => {
       price: 10,
       category: Category.ADVENTURE,
     };
-    // await controller.create(bookDto);
-    expect(controller.create).toHaveBeenCalledWith(bookDto);
-    expect(service.create).toHaveBeenCalledWith(bookDto);
+    await controller.create(bookDto);
+    expect(service.create).toHaveBeenCalled();
   });
 
-  // it('should get all books', async () => {
-  //   const result = [];
-  //   service.findAll.mockResolvedValue(result);
-  //   expect(await controller.findAll()).toBe(result);
-  // });
+  it('should get all books', async () => {
+    await controller.findAll();
+    expect(service.findAll).toHaveBeenCalled();
+  });
 
-  // it('should get one book', async () => {
-  //   const result = { id: 1, title: 'Test Book', author: 'Test Author' };
-  //   service.findOne.mockResolvedValue(result);
-  //   expect(await controller.findOne('1')).toBe(result);
-  // });
+  it('should get one book', async () => {
+    const id = '1';
+    await controller.findOne(id);
+    expect(service.findOne).toHaveBeenCalled();
+  });
 
-  // it('should update a book', async () => {
-  //   const bookDto = { title: 'Updated Book', author: 'Updated Author' };
-  //   service.update.mockResolvedValue(bookDto);
-  //   expect(await controller.update('1', bookDto)).toBe(bookDto);
-  // });
+  it('should update a book', async () => {
+    const id = '1';
+    const bookDto: UpdateBookDto = {
+      title: 'Updated Book',
+      author: 'Updated Author',
+      description: 'Updated Description',
+      price: 20,
+      category: Category.CLASSICS,
+    };
+    await controller.update(id, bookDto);
+    expect(service.update).toHaveBeenCalled();
+  });
 
-  // it('should delete a book', async () => {
-  //   service.remove.mockResolvedValue({ deleted: true });
-  //   expect(await controller.remove('1')).toEqual({ deleted: true });
-  // });
+  it('should delete a book', async () => {
+    const id = '1';
+    await controller.remove(id);
+    expect(service.remove).toHaveBeenCalled();
+  });
 });
+
+/*
+await controller.create(bookDto);
+  - call actual controller
+  - call mock service (can define a return value for the mock function)
+*/
