b971dfd99fc9438813d29b32e8ea565655b9e70a
Kyaw Ye Htet <kyawyehtet@amdon.com>
2024-06-13 09:48:12 +0630

    book service testcase

--- src/book/book.service.spec.ts
+++ src/book/book.service.spec.ts
@@ -1,18 +1,80 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookService } from './book.service';
+import { BookSchema, Category } from './schemas/book.schema';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { MongooseModule } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
 
 describe('BookService', () => {
   let service: BookService;
+  let mongod: MongoMemoryServer;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongooseModule.forRoot(uri),
+        MongooseModule.forFeature([{ name: 'Book', schema: BookSchema }]),
+      ],
       providers: [BookService],
     }).compile();
 
     service = module.get<BookService>(BookService);
   });
 
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongod.stop();
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('CRUD operations', () => {
+    let bookId: string;
+    let updatedBookDto: any;
+    const bookDto = {
+      title: 'Test Book',
+      description: 'Test Description',
+      author: 'Test Author',
+      price: 123,
+      category: Category.ADVENTURE,
+    };
+
+    it('should create a book', async () => {
+      const book = await service.create(bookDto);
+      expect(book).toMatchObject(bookDto);
+      bookId = book.id;
+    });
+
+    it('should get all books', async () => {
+      const books = await service.findAll();
+      expect(books).toMatchObject([bookDto]);
+    });
+
+    it('should get a book', async () => {
+      const book = await service.findOne(bookId);
+      expect(book).toMatchObject(bookDto);
+    });
+
+    it('should update a book', async () => {
+      const updateBookDto = {
+        title: 'Updated Book',
+      };
+      const updatedBook = await service.update(bookId, updateBookDto);
+      updatedBookDto = {
+        ...bookDto,
+        ...updateBookDto,
+      };
+      expect(updatedBook).toMatchObject(updatedBookDto);
+    });
+
+    it('should delete a book', async () => {
+      const deletedBook = await service.remove(bookId);
+      expect(deletedBook).toMatchObject(updatedBookDto);
+    });
   });
 });
